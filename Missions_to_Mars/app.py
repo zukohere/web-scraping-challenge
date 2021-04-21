b2dc74b764691477720e433cd20ce9aca0e5c235
Matt Brown <matthewbrown84@gmail.com>
2021-04-21 15:15:07 -0400

    more flask and html, 2nd row

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
+app = Flask(__name__)
+
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
